b2528b7e4f4c6371db5f5d86fe2d09c9f7a624f0
Spimy <officialspyme@gmail.com>
2023-12-12 15:19:49 +0800

    📦 NEW: Add firebase info to env interface

--- backend/src/global.d.ts
+++ backend/src/global.d.ts
@@ -1,10 +1,17 @@
 declare global {
-	namespace NodeJS {
-		interface ProcessEnv {
-			PORT: string;
-			CORS: string;
-		}
-	}
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      CORS: string;
+      FIREBASE_API_KEY: string;
+      FIREBASE_AUTH_DOMAIN: string;
+      FIREBASE_PROJECT_ID: string;
+      FIREBASE_STORAGE_BUCKET: string;
+      FIREBASE_MESSAGING_SENDER_ID: string;
+      FIREBASE_APP_ID: string;
+      FIREBASE_MEASUREMENT_ID: string;
+    }
+  }
 }
 
 export {};
