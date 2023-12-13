24c08186af25cc0163284e3844aa1b3d6355ed85
Spimy <officialspyme@gmail.com>
2023-12-13 16:55:06 +0800

    👌 IMPROVE: Remove routes and middleware

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,10 +1,13 @@
-import { Router } from 'express';
-import { login, register } from '../controllers/auth.controller';
-import { hasRequiredBody } from '../middleware/auth.middleware';
+import { Response, Router } from 'express';
+import { firebase } from '../firebase';
 
 const router = Router();
 
-router.post('/auth/register', hasRequiredBody, register);
-router.post('/auth/login', hasRequiredBody, login);
+router.get('/auth/test', async (_, res: Response) => {
+  const user = await firebase
+    .auth()
+    .getUserByEmail('williamlaw.3001@gmail.com');
+  res.status(200).json({ message: 'testing authentication', user });
+});
 
 export default router;
