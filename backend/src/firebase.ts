405530f2435b33259461caf1c67986460aa4e512
Spimy <officialspyme@gmail.com>
2023-12-13 16:55:19 +0800

    👌 IMPROVE: Update firebase intialisation

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,22 +1,26 @@
 import dotenv from 'dotenv';
-import { FirebaseOptions, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import admin, { type ServiceAccount } from 'firebase-admin';
 
 dotenv.config();
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+} as ServiceAccount;
 
-const firebase = initializeApp(firebaseConfig);
-const database = getFirestore(firebase);
-const auth = getAuth(firebase);
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const database = firebase.firestore();
+const auth = firebase.auth();
 
 export { auth, database, firebase };
--- backend/src/global.d.ts
+++ backend/src/global.d.ts
@@ -3,13 +3,17 @@ declare global {
     interface ProcessEnv {
       PORT: string;
       CORS: string;
-      FIREBASE_API_KEY: string;
-      FIREBASE_AUTH_DOMAIN: string;
+      FIREBASE_TYPE: string;
       FIREBASE_PROJECT_ID: string;
-      FIREBASE_STORAGE_BUCKET: string;
-      FIREBASE_MESSAGING_SENDER_ID: string;
-      FIREBASE_APP_ID: string;
-      FIREBASE_MEASUREMENT_ID: string;
+      FIREBASE_PRIVATE_KEY_ID: string;
+      FIREBASE_PRIVATE_KEY: string;
+      FIREBASE_CLIENT_EMAIL: string;
+      FIREBASE_CLIENT_ID: string;
+      FIREBASE_AUTH_URI: string;
+      FIREBASE_TOKEN_URI: string;
+      FIREBASE_AUTH_PROVIDER_CERT_URL: string;
+      FIREBASE_CLIENT_CERT_URL: string;
+      FIREBASE_UNIVERSE_DOMAIN: string;
     }
   }
 }
