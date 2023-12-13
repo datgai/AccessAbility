8adef213a61d467794f9820de7996fcb3d1e626d
Spimy <officialspyme@gmail.com>
2023-12-13 14:46:42 +0800

    👌 IMPROVE: Use body check middleware

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { register } from '../controllers/auth.controller';
+import { hasRequiredBody } from '../middleware/auth.middleware';
 
 const router = Router();
 
-router.post('/auth', register);
+router.post('/auth', hasRequiredBody, register);
 
 export default router;
