d801c43cae305e1d715f35062ee8ca4d1c398a8c
Spimy <officialspyme@gmail.com>
2023-12-15 18:09:55 +0800

    📦 NEW: Add routes for login and register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
