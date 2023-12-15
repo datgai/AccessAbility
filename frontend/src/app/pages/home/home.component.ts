0dc5ea4c2b60f9c2ff3740b0d1eb3d06d9f2f611
Spimy <officialspyme@gmail.com>
2023-12-15 22:44:50 +0800

    📦 NEW: Add test api endpoint form submission

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
-import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { Auth, User, getIdToken, onAuthStateChanged } from '@angular/fire/auth';
 import { FormGroup, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthenticationService } from '../../shared/authentication.service';
+import { TestService } from './services/test.service';
 
 @Component({
   selector: 'app-home',
@@ -15,10 +17,13 @@ import { AuthenticationService } from '../../shared/authentication.service';
 export class HomeComponent implements OnInit {
   public isAuthenticated = false;
   public logoutForm!: FormGroup;
+  public responseMessage: string | undefined = undefined;
+  public user: User | undefined = undefined;
 
   constructor(
     private auth: Auth = inject(Auth),
     private authenticationService: AuthenticationService,
+    private testService: TestService,
     private router: Router
   ) {}
 
@@ -29,7 +34,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  onSubmit(): void {
+  onSubmitLogout(): void {
     const sub: Subscription = this.authenticationService.logout().subscribe({
       next: () => {
         localStorage.removeItem(this.authenticationService.userKey);
@@ -40,6 +45,22 @@ export class HomeComponent implements OnInit {
         sub.unsubscribe();
       },
       complete: () => sub.unsubscribe(),
+    });
+  }
+
+  async onSubmit() {
+    const token = this.auth.currentUser
+      ? await getIdToken(this.auth.currentUser)
+      : '';
+
+    this.testService.getTest(token).subscribe({
+      next: (response) => {
+        this.responseMessage = response.message;
+        this.user = response.user;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.responseMessage = error.error.message as string;
+      },
     });
   }
 }
