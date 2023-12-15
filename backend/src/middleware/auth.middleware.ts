6a0080b8973a3f60b22cf4499cb79cc9bacf22e9
Spimy <officialspyme@gmail.com>
2023-12-15 22:42:52 +0800

    📦 NEW: Add middleware to check if user is authenticated

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,1 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { auth } from '../firebase';
 
+export const isAuthenticated = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log(request.headers.authorization);
+  const idToken = request.headers.authorization?.split('Bearer ')[1] || '';
+
+  await auth
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      request.user = decodedToken;
+      next();
+    })
+    .catch(() => {
+      return response.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'You are not authorized to access this endpoint.'
+      });
+    });
+};
