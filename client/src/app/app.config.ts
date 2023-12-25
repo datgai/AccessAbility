bd11b2b3f563ef687723043f3a46f04302a31ac2
Spimy <officialspyme@gmail.com>
2023-12-25 16:55:29 +0800

    👌 IMPROVE: Remove SSR and rename to client

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  ScreenTrackingService,
+  UserTrackingService,
+  getAnalytics,
+  provideAnalytics,
+} from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    provideHttpClient(withFetch()),
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
+};
---
+++ client/src/app/pages/home/home.component.ts
@@ -0,0 +1,64 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Auth, User, getIdToken } from '@angular/fire/auth';
+import { FormGroup, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from '../../shared/authentication.service';
+import { TestService } from './services/test.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  public logoutForm!: FormGroup;
+  public responseMessage: string | undefined = undefined;
+  public user: User | undefined = undefined;
+
+  public isAuthenticated: boolean = localStorage.getItem(
+    this.authenticationService.userKey
+  )
+    ? true
+    : false;
+
+  constructor(
+    private auth: Auth = inject(Auth),
+    private authenticationService: AuthenticationService,
+    private testService: TestService,
+    private router: Router
+  ) {}
+
+  onSubmitLogout(): void {
+    const sub: Subscription = this.authenticationService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem(this.authenticationService.userKey);
+        this.router.navigate(['login']);
+      },
+      error: (error: Error) => {
+        console.log(error.message);
+        sub.unsubscribe();
+      },
+      complete: () => sub.unsubscribe(),
+    });
+  }
+
+  async onSubmit() {
+    const token = this.auth.currentUser
+      ? await getIdToken(this.auth.currentUser)
+      : '';
+
+    this.testService.getTest(token).subscribe({
+      next: (response) => {
+        this.responseMessage = response.message;
+        this.user = response.user;
+      },
+      error: (response: HttpErrorResponse) => {
+        this.responseMessage = response.error.message as string;
+      },
+    });
+  }
+}
