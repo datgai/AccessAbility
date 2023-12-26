4cb7f6d6793e8f7a7289fe29b3f516227c6f1b42
Spimy <officialspyme@gmail.com>
2023-12-26 19:35:59 +0800

    📦 NEW: Add endpoint to get user profile by id

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -2,10 +2,34 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { UserProfile, UserRole } from '../../../shared/src/types/user';
 import { profilesRef } from '../database';
+import { auth } from '../firebase';
 
 export const getProfile = async (request: Request, response: Response) => {
   const user = request.user;
   return response.status(StatusCodes.OK).json({ profile: user.profile });
+};
+
+export const getProfileById = async (request: Request, response: Response) => {
+  const userId = request.params.id ?? '';
+
+  await auth
+    .getUser(userId)
+    .then(async (user) => {
+      const profile = await profilesRef.doc(user.uid).get();
+
+      return response.status(StatusCodes.OK).json({
+        message: 'User found.',
+        user: {
+          ...user,
+          profile: profile.exists ? profile.data() ?? {} : {}
+        }
+      });
+    })
+    .catch(() => {
+      return response.status(StatusCodes.NOT_FOUND).json({
+        message: 'User you are looking for cannot be found.'
+      });
+    });
 };
 
 export const createProfile = async (request: Request, response: Response) => {
--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { createProfile, getProfile } from '../controllers/users.controller';
+import {
+  createProfile,
+  getProfile,
+  getProfileById
+} from '../controllers/users.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.get('/users/profile', isAuthenticated, getProfile);
+router.get('/users/profile/:id', getProfileById);
 router.post('/users/profile', isAuthenticated, createProfile);
 
 export default router;
