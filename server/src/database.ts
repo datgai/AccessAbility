5d0a5047472b8b9a008ad2e71b40a0899961289c
Spimy <officialspyme@gmail.com>
2023-12-26 19:54:31 +0800

    📦 NEW: Add utility function to get profile

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,3 +1,9 @@
+import { UserProfile } from '../../shared/src/types/user';
 import { database } from './firebase';
 
 export const profilesRef = database.collection('profiles');
+
+export const getProfileById = async (userId: string) => {
+  const profile = await profilesRef.doc(userId).get();
+  return (profile.exists ? profile.data() ?? {} : {}) as UserProfile;
+};
