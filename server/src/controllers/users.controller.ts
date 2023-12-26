c271fb1001879355a38aa4bcc9dfed6d64ab343f
Spimy <officialspyme@gmail.com>
2023-12-26 19:01:34 +0800

    📦 NEW: Add route and controller to get profile

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -3,6 +3,11 @@ import { StatusCodes } from 'http-status-codes';
 import { UserProfile, UserRole } from '../../../shared/src/types/user';
 import { profilesRef } from '../database';
 
+export const getProfile = async (request: Request, response: Response) => {
+  const user = request.user;
+  return response.status(StatusCodes.OK).json({ profile: user.profile });
+};
+
 export const createProfile = async (request: Request, response: Response) => {
   const user = request.user;
   const userRole: UserRole = request.body.role;
@@ -16,6 +21,6 @@ export const createProfile = async (request: Request, response: Response) => {
 
   return response.status(StatusCodes.CREATED).json({
     message: 'Profile added successfully.',
-    user: { ...user, ...profileData }
+    user: { ...user, profile: profileData }
   });
 };
--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createProfile } from '../controllers/users.controller';
+import { createProfile, getProfile } from '../controllers/users.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const router = Router();
 
+router.get('/users/profile', isAuthenticated, getProfile);
 router.post('/users/profile', isAuthenticated, createProfile);
 
 export default router;
