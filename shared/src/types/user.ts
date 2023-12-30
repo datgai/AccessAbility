e4e0507eb7960b4cac42ea0bf4b77ae117d2dd07
Spimy <officialspyme@gmail.com>
2023-12-30 19:53:59 +0800

    👌 IMPROVE: Update user profile to contain all info

--- shared/src/types/user.ts
+++ shared/src/types/user.ts
@@ -4,7 +4,24 @@ export enum UserRole {
 	ADMIN = 'admin',
 }
 
+export enum UserGender {
+	MALE = 'male',
+	FEMALE = 'female',
+	OTHER = 'other',
+}
+
 export interface UserProfile {
+	firstName: string;
+	lastName: string;
+	gender: UserGender;
+	dateOfBirth: Date;
+	phoneNumber: string;
+	impairments: string[];
+	city: string;
+	state: string;
+	address: string;
+	bio: string;
+	profilePictureUrl: string;
 	role: UserRole;
 	premium: boolean;
 }
