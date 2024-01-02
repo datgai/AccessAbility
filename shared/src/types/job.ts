e4f3676a3557654a5381c759af4820a91d1c93db
Spimy <officialspyme@gmail.com>
2024-01-02 18:02:02 +0800

    📦 NEW: Add share job types

---
+++ shared/src/types/job.ts
@@ -0,0 +1,23 @@
+export enum JobType {
+	FULL_TIME = 'full-time',
+	PART_TIME = 'part-time',
+	CONTRACT = 'contract',
+	INTERNSHIP = 'internship',
+	APPRENTICESHIP = 'apprenticeship',
+	SEASONAL = 'seasonal',
+}
+
+export enum JobLocationType {
+	ON_SITE = 'on-site',
+	HYBRID = 'hybrid',
+	REMOTE = 'remote',
+}
+
+export interface Job {
+	businessId: string;
+	position: string;
+	type: JobType;
+	locationType: JobLocationType;
+	description: string;
+	createdAt: Date;
+}
