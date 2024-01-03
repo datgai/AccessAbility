7cff53da74adc80d6592586a86d683ebac451092
Spimy <officialspyme@gmail.com>
2024-01-04 01:17:31 +0800

    📦 NEW: Add business page view

---
+++ client/src/app/views/home/business-view/business-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './business-view.component.html',
+  styleUrl: './business-view.component.css'
+})
+export class BusinessViewComponent {
+
+}
