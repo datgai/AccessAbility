6e38e9e7badb83b72194d074f7a8c8c20c09b87d
Spimy <officialspyme@gmail.com>
2024-01-04 01:20:57 +0800

    📦 NEW: Import view components

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,35 +1,47 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Auth, User, getIdToken } from '@angular/fire/auth';
 import { FormGroup, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthenticationService } from '../../shared/authentication.service';
+import { UserProfile, UserRole } from '../../../../../shared/src/types/user';
 import { TestService } from '../../service/test.service';
+import { AuthenticationService } from '../../shared/authentication.service';
+import { BusinessViewComponent } from '../../views/home/business-view/business-view.component';
+import { LandingViewComponent } from '../../views/home/landing-view/landing-view.component';
+import { UserViewComponent } from '../../views/home/user-view/user-view.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    LandingViewComponent,
+    UserViewComponent,
+    BusinessViewComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public logoutForm!: FormGroup;
   public responseMessage: string | undefined = undefined;
-  public user: User | undefined = undefined;
+  public UserRole = UserRole;
 
-  public isAuthenticated: boolean = localStorage.getItem(
-    this.authenticationService.userKey
-  )
-    ? true
-    : false;
+  public user: (User & { profile: UserProfile }) | undefined = undefined;
+  public isAuthenticated!: boolean;
 
   constructor(
     private auth: Auth = inject(Auth),
     private authenticationService: AuthenticationService,
     private testService: TestService,
-    private router: Router
+    private router: Router,
   ) {}
+
+  ngOnInit(): void {
+    const user = localStorage.getItem(this.authenticationService.userKey) ?? '';
+    this.isAuthenticated = user ? true : false;
+    this.user = user ? JSON.parse(user) : undefined;
+  }
 
   onSubmitLogout(): void {
     this.authenticationService.logout().subscribe({
@@ -49,7 +61,7 @@ export class HomeComponent {
     this.testService.getTest(token).subscribe({
       next: (response) => {
         this.responseMessage = response.message;
-        this.user = response.user;
+        this.user = response.user as User & { profile: UserProfile };
       },
       error: (response: HttpErrorResponse) => {
         this.responseMessage = response.error.message as string;
