cb2abc72c6cadb8a3ffa84583b5c6fa368860d51
Spimy <officialspyme@gmail.com>
2024-01-04 17:32:22 +0800

    👌 IMPROVE: Add colour variable to blue

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
- 
+  content: ["./src/**/*.{html,ts}"],
+
   theme: {
     extend: {
       colors: {
-        blue: '#267FFF'
-      }
+        blue: {
+          100: "hsl(215, 100%, 57%)",
+          200: "hsl(215, 100%, 50%)",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
