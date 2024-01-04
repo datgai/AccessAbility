dc911d0fa55b94c5c5aa34e4489e478f38ee6c1e
Spimy <officialspyme@gmail.com>
2024-01-04 23:05:17 +0800

    👌 IMPROVE: Show logout button in nav if authenticated

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { UserStoreService } from '../../services/user-store.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(
+    public userStore: UserStoreService,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) {}
 
+  onSubmitLogout(): void {
+    this.authenticationService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem(this.authenticationService.userKey);
+        this.router.navigate(['login']);
+      },
+      error: (error: Error) => console.log(error.message),
+    });
+  }
 }
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,10 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { Auth, getIdToken } from '@angular/fire/auth';
-import { FormGroup, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { UserRole } from '../../../../../shared/src/types/user';
-import { AuthenticationService } from '../../services/authentication.service';
 import { TestService } from '../../services/test.service';
 import { UserStoreService } from '../../services/user-store.service';
 import { BusinessViewComponent } from '../../views/home/business-view/business-view.component';
@@ -14,37 +11,19 @@ import { UserViewComponent } from '../../views/home/user-view/user-view.componen
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    FormsModule,
-    LandingViewComponent,
-    UserViewComponent,
-    BusinessViewComponent,
-  ],
+  imports: [LandingViewComponent, UserViewComponent, BusinessViewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  public logoutForm!: FormGroup;
   public responseMessage: string | undefined = undefined;
   public UserRole = UserRole;
 
   constructor(
     private auth: Auth = inject(Auth),
-    private authenticationService: AuthenticationService,
     private testService: TestService,
-    private router: Router,
     public userStore: UserStoreService,
   ) {}
-
-  onSubmitLogout(): void {
-    this.authenticationService.logout().subscribe({
-      next: () => {
-        localStorage.removeItem(this.authenticationService.userKey);
-        this.router.navigate(['login']);
-      },
-      error: (error: Error) => console.log(error.message),
-    });
-  }
 
   async onSubmit() {
     const token = this.auth.currentUser
