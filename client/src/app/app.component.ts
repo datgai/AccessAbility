ca55aac8bc54c017d4cce8004e308e744fb41d9a
Spimy <officialspyme@gmail.com>
2024-01-05 17:16:18 +0800

    📦 NEW: Keep localstorage in sync with user profile

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserStoreService } from './services/user-store.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +19,20 @@ import { HeaderComponent } from './components/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private userStore: UserStoreService,
+  ) {}
+
+  // Keep local storage in sync when loading the application
+  ngOnInit(): void {
+    // Delete user from local storage if not logged in
+    if (!this.userStore.isAuthenticated()) return this.userStore.deleteUser();
+
+    // Update profile
+    this.userService.getUser(this.userStore.user?.uid ?? '').subscribe({
+      next: (user) => (this.userStore.user = user),
+    });
+  }
 }
