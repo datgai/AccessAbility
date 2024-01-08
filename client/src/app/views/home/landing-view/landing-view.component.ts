b28cc482185192284025d4d7a58f1f060a06332a
rachels-archive <rachelhui@protonmail.com>
2024-01-08 10:58:36 +0800

    🐛 👌 IMPROVE: Change landing page to match Figma

--- client/src/app/views/home/landing-view/landing-view.component.ts
+++ client/src/app/views/home/landing-view/landing-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing-view.component.html',
   styleUrl: './landing-view.component.css'
 })
