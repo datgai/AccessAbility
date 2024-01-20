6c8ddb2a1150f44976132c007a2544c66758738c
Spimy <officialspyme@gmail.com>
2024-01-20 16:59:42 +0800

    📦 NEW: Add get user and profile method

--- server/src/utils/user.util.ts
+++ server/src/utils/user.util.ts
@@ -1,7 +1,15 @@
 import { UserProfile } from '../../../shared/src/types/user';
 import { profilesRef } from '../database';
+import { auth } from '../firebase';
 
 export const getProfileById = async (userId: string) => {
   const profile = await profilesRef.doc(userId).get();
   return (profile.exists ? profile.data() ?? {} : {}) as UserProfile;
+};
+
+export const getUserAndProfile = async (userId: string) => {
+  return await auth.getUser(userId).then(async (business) => {
+    const profile = await getProfileById(business.uid);
+    return { ...business, profile };
+  });
 };
