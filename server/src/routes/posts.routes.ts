9b9712f492e6651959056ade07d1854c48fb2810
Spimy <officialspyme@gmail.com>
2024-01-20 17:56:52 +0800

    🐛 FIX: Add get posts controller to get posts route

--- server/src/routes/posts.routes.ts
+++ server/src/routes/posts.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createPost, deletePostById } from '../controllers/posts.controller';
+import { createPost, deletePostById, getPosts } from '../controllers/posts.controller';
 import { isAuthenticated } from '../middleware/auth.middleware';
 
 const router = Router();
 
-router.get('/posts');
+router.get('/posts', getPosts);
 router.post('/post', isAuthenticated, createPost);
 router.delete('/post/:id', isAuthenticated, deletePostById);
 
