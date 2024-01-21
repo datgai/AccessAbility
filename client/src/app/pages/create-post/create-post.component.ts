8e981bf1a9af40888a4c5f7ddee4e50938b57fcd
rachels-archive <rachelhui@protonmail.com>
2024-01-21 14:38:05 +0800

    📦 NEW: Connect frontend to backend in forum service

--- client/src/app/pages/create-post/create-post.component.ts
+++ client/src/app/pages/create-post/create-post.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { ForumService } from '../../services/forum.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
 })
 export class CreatePostComponent {
- 
+
+  createPostForm: FormGroup = new FormGroup(
+    {
+      postTitle: new FormControl('', [Validators.required]),
+      postContent: new FormControl('', [Validators.required]),
+      thumbnailUrl: new FormControl('', [Validators.required]),
+      isDonation: new FormControl(false, [Validators.required])
+  }
+  );
+
+  constructor(
+    private httpClient: HttpClient,
+    private forumService: ForumService
+  ) {}
+
+  onSubmit() {
+    
+  }
+
+
+
 }
--- client/src/app/services/forum.service.ts
+++ client/src/app/services/forum.service.ts
@@ -1,10 +1,64 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../../../shared/src/types/post'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError, from } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Component, inject } from '@angular/core';
+import { Auth, getIdToken } from '@angular/fire/auth';
+import { UserStoreService } from './user-store.service';
+import { switchMap, catchError } from 'rxjs/operators';
+import { UserService } from './user.service';
+
+
+export type PostDetails = Post & { id: string };
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForumService {
 
+  constructor(
+    private http: HttpClient, 
+    private auth: Auth = inject(Auth)
+    ) {}
+
+    createPost(body: {
+      title: string;
+      content: string;
+      image: string;
+      isDonation: boolean;
+    }): Observable<PostDetails> {
+      const currentUser = this.auth.currentUser;
+  
+      if (!currentUser) {
+        return throwError(() => 'Current user is undefined.');
+      }
+  
+      return from(currentUser.getIdToken()).pipe(
+        switchMap(token => {
+          if (!token) {
+            return throwError(() => 'Authentication token is missing.');
+          }
+
+          const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+          return this.http.post<PostDetails>(`${environment.baseUrl}/post`, body, { headers });
+        }),
+        catchError(error => {
+          return throwError(() => error);
+        })
+      );
+    }
+
+    getPostList(): Observable<PostDetails[]> {
+      return this.http.get<PostDetails[]>(`${environment.baseUrl}/posts`);
+    }
+  
+    getPost(postId: string) {
+      return this.http.get<PostDetails>(`${environment.baseUrl}/posts/${postId}`);
+    }
+
+    deletePost(postId: string): Observable<void> {
+      return this.http.delete<void>(`${environment.baseUrl}/post/${postId}`);
+    }
   
 }
