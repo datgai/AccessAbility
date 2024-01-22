5e8ec8612efed4a1b7aff0dbc1691b1c7dc031ac
Spimy <officialspyme@gmail.com>
2024-01-23 03:42:15 +0800

    👌 IMPROVE: Show offer received button if user has received offer from the business

--- client/src/app/pages/job-details/job-details.component.ts
+++ client/src/app/pages/job-details/job-details.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MarkdownPipe } from 'ngx-markdown';
+import { map } from 'rxjs';
 import { LoaderComponent } from '../../components/loader/loader.component';
 import { MiniInfoCardComponent } from '../../components/mini-info-card/mini-info-card.component';
 import { JobDetails, JobService } from '../../services/job.service';
 import { UserStoreService } from '../../services/user-store.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-job-details',
@@ -24,17 +27,33 @@ import { UserStoreService } from '../../services/user-store.service';
 })
 export class JobDetailsComponent implements OnInit {
   public job = signal<JobDetails | undefined>(undefined);
+  public userOfferedBusinesses = signal<string[]>([]);
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private userService: UserService,
     private jobService: JobService,
+    private auth: Auth,
     public userStore: UserStoreService,
   ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (!id) return this.router.navigate(['404']);
+
+    this.auth.onAuthStateChanged(async (user) => {
+      if (!user) return;
+      const token = await user.getIdToken();
+
+      this.userService
+        .getUserOffers(token, user.uid)
+        .pipe(map((offers) => offers.map((offer) => offer.business.uid)))
+        .subscribe({
+          next: (businessIds) => this.userOfferedBusinesses.set(businessIds),
+          complete: () => console.log(this.userOfferedBusinesses()),
+        });
+    });
 
     this.jobService.getJob(id).subscribe({
       next: (response) => this.job.set(response),
