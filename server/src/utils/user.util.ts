d963b49c3e163200edab33ebbfbc5cef32d8288b
Spimy <officialspyme@gmail.com>
2024-01-22 22:08:46 +0800

    👌 IMPROVE: Refactor get profile id

--- server/src/utils/user.util.ts
+++ server/src/utils/user.util.ts
@@ -1,24 +1,38 @@
 import { firestore } from 'firebase-admin';
+import { Skill } from '../../../shared/src/types/job';
 import { UserProfile } from '../../../shared/src/types/user';
-import { profilesRef } from '../database';
+import { profilesRef, skillsRef } from '../database';
 import { auth } from '../firebase';
 
-export const getProfileById = async (userId: string) => {
+export const getProfileById = async (userId: string): Promise<UserProfile> => {
+  // Fetch profile
   const profile = (await profilesRef
     .doc(userId)
     .get()) as GenericDocument<UserProfile>;
 
-  const { dateOfBirth, ...profileData } = profile.data();
-  if (!dateOfBirth) return {} as UserProfile;
+  // Check if profile exists
+  if (!profile.exists) return {} as UserProfile;
 
-  return (
-    profile.exists
-      ? {
-          ...profileData,
-          dateOfBirth: (dateOfBirth as unknown as firestore.Timestamp).toDate()
-        } ?? {}
-      : {}
-  ) as UserProfile;
+  // Extract data that needs to be converted or populated
+  const { dateOfBirth, skills, ...profileData } = profile.data();
+
+  // Get the skill name of each skill
+  const populatedSkills = await Promise.all(
+    skills.map(async (skillId) => {
+      const skill = (await skillsRef
+        .doc(skillId)
+        .get()) as GenericDocument<Skill>;
+      return skill.data().name;
+    })
+  );
+
+  const populatedProfile: UserProfile = {
+    ...profileData,
+    skills: populatedSkills,
+    dateOfBirth: (dateOfBirth as unknown as firestore.Timestamp).toDate()
+  };
+
+  return populatedProfile;
 };
 
 export const getUserAndProfile = async (userId: string) => {
