efd399648842c33eb274c23fe8590452ddf507e3
Spimy <officialspyme@gmail.com>
2024-01-23 16:56:19 +0800

    👌 IMPROVE: Add loader

--- client/src/app/pages/forum/forum.component.ts
+++ client/src/app/pages/forum/forum.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from '../../components/loader/loader.component';
 import { PostComponent } from '../../components/post/post.component';
 import { ForumService, PostDetails } from '../../services/forum.service';
 import { UserStoreService } from '../../services/user-store.service';
@@ -8,28 +9,26 @@ import { UserStoreService } from '../../services/user-store.service';
 @Component({
   selector: 'app-forum',
   standalone: true,
-  imports: [PostComponent, CommonModule, RouterModule],
+  imports: [PostComponent, CommonModule, RouterModule, LoaderComponent],
   templateUrl: './forum.component.html',
-  styleUrl: './forum.component.css'
+  styleUrl: './forum.component.css',
 })
-
-export class ForumComponent implements OnInit{
+export class ForumComponent implements OnInit {
   postList: PostDetails[] = [];
   forumService: ForumService = inject(ForumService);
 
   constructor(public userStore: UserStoreService) {}
 
   ngOnInit(): void {
-    this.loadPosts()
+    this.loadPosts();
   }
 
   loadPosts() {
-  this.forumService.getPosts().subscribe({
-    next: (response) => {
-      this.postList = response.posts
-    },
-    error: (err) => console.error(err),
-  });
-}
-
+    this.forumService.getPosts().subscribe({
+      next: (response) => {
+        this.postList = response.posts;
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
