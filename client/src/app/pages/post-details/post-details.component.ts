7a1ffcaa340fa9e21b85aa71c69c7c5565a64921
Spimy <officialspyme@gmail.com>
2024-01-23 15:49:34 +0800

    👌 IMPROVE: Refactor deprecated subscribe function

--- client/src/app/pages/post-details/post-details.component.ts
+++ client/src/app/pages/post-details/post-details.component.ts
@@ -1,43 +1,41 @@
-import { Component,OnInit,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from '../../../../../shared/src/types/post';
-import { ForumService } from '../../services/forum.service';
 import { CommentComponent } from '../../components/comment/comment.component';
+import { ForumService } from '../../services/forum.service';
 
 @Component({
   selector: 'app-post-details',
   standalone: true,
   imports: [CommonModule, CommentComponent],
   templateUrl: './post-details.component.html',
-  styleUrl: './post-details.component.css'
+  styleUrl: './post-details.component.css',
 })
-export class PostDetailsComponent implements OnInit{
-  public postId: string = '' 
+export class PostDetailsComponent implements OnInit {
+  public postId: string = '';
 
   forumService = inject(ForumService);
   post: Post | undefined;
 
-  constructor(private route:ActivatedRoute){}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const params = this.route.snapshot.params;
     this.postId = params['id'];
-    
-    this.forumService.getPostById(this.postId).subscribe(
-      (post) => {
+
+    this.forumService.getPostById(this.postId).subscribe({
+      next: (post) => {
         this.post = post;
         console.log('Post loaded successfully:', this.post);
       },
-      (error) => {
+      error: (error) => {
         console.error('Error loading post:', error);
-      }
-    );
+      },
+    });
   }
 
   formatDate(date: Date | undefined): string {
     return (date ? new Date(date) : new Date()).toLocaleDateString();
   }
-  
-  
 }
