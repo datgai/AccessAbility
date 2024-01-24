d18c13cdcc0767cdb09ed55b83e00af4e9a50a8b
Spimy <officialspyme@gmail.com>
2024-01-24 17:11:05 +0800

    📦 NEW: Add resources reference

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,7 +1,7 @@
 import { database } from './firebase';
 
 export const profilesRef = database.collection('profiles');
-
 export const postsRef = database.collection('posts');
 export const jobsRef = database.collection('jobs');
 export const skillsRef = database.collection('skills');
+export const resourcesRef = database.collection('resources');
