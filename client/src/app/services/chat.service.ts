e00ef840c6a4d2777354d2b0f7eda5aab127c122
Alex Chee <xxchee555xx@gmail.com>
2024-01-25 22:19:30 +0800

    🚀 IMPROVE : Add new services for chats

--- client/src/app/services/chat.service.ts
+++ client/src/app/services/chat.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { UserDetails } from './user.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { catchError, from, switchMap, throwError } from 'rxjs';
+import { Observable, catchError, from, switchMap, throwError } from 'rxjs';
 import { Auth } from '@angular/fire/auth';
+import { Firestore, collection, collectionData, orderBy, query } from '@angular/fire/firestore';
 
-export interface Chat {
-  cid: string;
+export interface ChatDetails {
+  id: string;
   lastMessage?:string;
   lastMessageDate?:Date;
   userIds: string[];
@@ -17,12 +18,9 @@ export interface Chat {
   chatName?: string;
 }
 
-export interface ChatDetails{
-  userIds: string[];
-  users:UserDetails[]
-}
 
 export interface Message{
+  id:string;
   text: string;
   senderId: string;
   sentDate: Date;
@@ -32,8 +30,10 @@ export interface Message{
   providedIn: 'root'
 })
 export class ChatService {
-  constructor(private http: HttpClient,private auth: Auth,) { }
+  firestore: Firestore = inject(Firestore);
 
+  constructor(private http: HttpClient,private auth: Auth) { }
+ 
   createChat(otherUser:UserDetails){
     const currentUser = this.auth.currentUser;
 
@@ -62,13 +62,52 @@ export class ChatService {
     );
   }
 
-  addChatNameAndPic(currentUserId: string, chats:Chat[]): Chat[]{
-    chats.forEach(chat => {
-      const otherIndex = chat.userIds.indexOf(currentUserId) === 0 ? 1 :0;
-      const {displayName, photoURL} = chat.users[otherIndex];
-      chat.chatName = displayName || '';
-      chat.chatPic = photoURL|| '';
-    })
-    return chats
+  getChats(userId: string){
+    return this.http.get<ChatDetails[]>(`${environment.baseUrl}/chats/${userId}`)
+  }
+
+
+  getChatById(chatId: string){
+    return this.http.get<ChatDetails>(`${environment.baseUrl}/chat/${chatId}`)
+  }
+
+  getMessages$(chatId: string) : Observable<Message[]>{
+    return this.http.get<Message[]>( `${environment.baseUrl}/message/${chatId}`)
+  }
+
+  loadMessages = (chatId: string) => {
+    // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(this.firestore, 'chats', chatId, 'message'), orderBy('sentDate', 'asc'));
+    // Start listening to the query.
+    console.log(collectionData(recentMessagesQuery));
+    return collectionData(recentMessagesQuery);
+  }
+  
+  sendMessage(chatId: string,message:String){
+    const currentUser = this.auth.currentUser;
+
+    if (!currentUser) {
+      return throwError(() => 'Current user is undefined.');
+    }
+
+    return from(currentUser.getIdToken()).pipe(
+      switchMap((token) => {
+        if (!token) {
+          return throwError(() => 'Authentication token is missing.');
+        }
+        const headers = new HttpHeaders().set(
+          'Authorization',
+          `Bearer ${token}`,
+        );
+        return this.http.post<Message>(
+          `${environment.baseUrl}/message/${chatId}`,
+          {content : message},
+          { headers },
+        );
+      }),
+      catchError((error) => {
+        return throwError(() => error);
+      }),
+    );
   }
 }
