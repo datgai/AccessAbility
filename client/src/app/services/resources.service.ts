19f118c2462e99153ebae2841fa8678f5a79e229
Spimy <officialspyme@gmail.com>
2024-01-25 17:33:42 +0800

    👌 IMPROVE: Add resource details type

--- client/src/app/services/resources.service.ts
+++ client/src/app/services/resources.service.ts
@@ -1,17 +1,25 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Resource } from '../../../../shared/src/types/resource';
 import { environment } from '../../environments/environment';
+import { UserDetails } from './user.service';
+
+export type ResourceDetails = Omit<Resource, 'authorId'> & {
+  id: string;
+  author: UserDetails;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResourcesService {
-  constructor(private http: HttpClient) {
-    // router.get('/resources/:token?', isAuthenticated, getResources);
-  }
+  constructor(private http: HttpClient) {}
 
   getResources(token: string, pageToken: string = '') {
-    return this.http.get(`${environment.baseUrl}/resources/${pageToken}`, {
+    return this.http.get<{
+      resources: ResourceDetails[];
+      nextPageToken: string;
+    }>(`${environment.baseUrl}/resources/${pageToken}`, {
       headers: new HttpHeaders().set('Authorization', `Bearer ${token}`),
     });
   }
