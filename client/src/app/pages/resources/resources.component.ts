2c39f8b88c2d88913f3e76f47c6e15f91814a93d
Spimy <officialspyme@gmail.com>
2024-01-25 17:34:07 +0800

    👌 IMPROVE: Set next page token and add loader

--- client/src/app/pages/resources/resources.component.ts
+++ client/src/app/pages/resources/resources.component.ts
@@ -1,31 +1,47 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { ResourcesService } from '../../services/resources.service';
-import { Auth } from '@angular/fire/auth';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { RouterLink } from '@angular/router';
 import { MarkdownModule, MarkdownPipe } from 'ngx-markdown';
+import { LoaderComponent } from '../../components/loader/loader.component';
+import {
+  ResourceDetails,
+  ResourcesService,
+} from '../../services/resources.service';
 
 @Component({
   selector: 'app-resources',
   standalone: true,
-  imports: [RouterLink, CommonModule, MarkdownPipe, MarkdownModule],
+  imports: [
+    RouterLink,
+    CommonModule,
+    MarkdownPipe,
+    MarkdownModule,
+    LoaderComponent,
+  ],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.css',
 })
 export class ResourcesComponent implements OnInit {
-  public resources = signal<any[]>([]);
+  public resources = signal<ResourceDetails[]>([]);
+  public nextPageToken = signal<string | undefined>('');
+  public loading = signal<boolean>(true);
+
   constructor(
     private resourcesService: ResourcesService,
     private auth: Auth,
   ) {}
+
   ngOnInit(): void {
     this.auth.onAuthStateChanged(async (user) => {
       if (!user) return;
       const token = await user.getIdToken();
       this.resourcesService.getResources(token).subscribe({
-        next: (response: any) => {
+        next: (response) => {
           this.resources.set(response.resources);
+          this.nextPageToken.set(response.nextPageToken);
         },
+        complete: () => this.loading.set(false),
       });
     });
   }
